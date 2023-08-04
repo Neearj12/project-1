e172b5407dcdc5bfb9c0c44c2bbf04b0bb4d6359
Neeraj <neerajjoshi9389798147@gmail.com>
2023-08-04 10:48:43 +0530

    no p tag

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,29 +1,26 @@
-
-const Hero=()=>{
-    return <main className="hero cantainer">
-        <div className="hero-content">
-            <h1>Your feet Deserve The Best</h1>
-            <p> try pull hello Lorem ipsum dolor sit a Lorem ipsum dolor sit amet consectetur awsome and good looking sozes adipisicing elit. Animi, consectetur. Explicabo, nisi? feel free and comfortable with the sozes</p>
-<div className="hero-button">
-    <button>
-        Shop_Now
-    </button>
-    <button className="secondary-btn">Category</button>
-</div>
-<div className="shopping">
-    <p>Also available on </p>
-    <div className="brand-icon">
-        <img src="/images/amazon.png" alt="emazon-logo" />
-        <img src="/images/flipkart.png" alt="flipkart-logo" />
-    </div>
-</div>
+const Hero = () => {
+  return (
+    <main className="hero cantainer">
+      <div className="hero-content">
+        <h1>Your feet Deserve The Best</h1>
+     
+        <div className="hero-button">
+          <button>Shop_Now</button>
+          <button className="secondary-btn">Category</button>
         </div>
-
-        <div className="hero-image">
-<img src="/images/hero-image.png" alt="hero-image" />
+        <div className="shopping">
+          <p>Also available on </p>
+          <div className="brand-icon">
+            <img src="/images/amazon.png" alt="emazon-logo" />
+            <img src="/images/flipkart.png" alt="flipkart-logo" />
+          </div>
         </div>
+      </div>
 
+      <div className="hero-image">
+        <img src="/images/hero-image.png" alt="hero-image" />
+      </div>
     </main>
-    ;
-}
+  );
+};
 export default Hero;
