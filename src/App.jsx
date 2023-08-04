3b9eaaeed6f7951a8e9f4b60d3cb63d6a4cea59a
Neeraj <neerajjoshi9389798147@gmail.com>
2023-08-04 10:21:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navigation from "./component/Navigation";
+import Hero from "./component/Hero";
+
+
+const App =()=>{
+
+return (<div> 
+<Navigation/>
+<Hero/>
+</div>
+
+)
+};
+export default App;
---
+++ src/component/Hero.jsx
@@ -0,0 +1,29 @@
+
+const Hero=()=>{
+    return <main className="hero cantainer">
+        <div className="hero-content">
+            <h1>Your feet Deserve The Best</h1>
+            <p>Lorem ipsum dolor sit a Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, consectetur. Explicabo, nisi? feel free and comfortable with the sozes</p>
+<div className="hero-button">
+    <button>
+        Shop_Now
+    </button>
+    <button className="secondary-btn">Category</button>
+</div>
+<div className="shopping">
+    <p>Also available on </p>
+    <div className="brand-icon">
+        <img src="/images/amazon.png" alt="emazon-logo" />
+        <img src="/images/flipkart.png" alt="flipkart-logo" />
+    </div>
+</div>
+        </div>
+
+        <div className="hero-image">
+<img src="/images/hero-image.png" alt="hero-image" />
+        </div>
+
+    </main>
+    ;
+}
+export default Hero;
